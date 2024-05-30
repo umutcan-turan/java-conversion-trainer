19f839a5a6c69d5cca16a4b53b89e6d9800708f1
Umutcan Turan <mail@umutcan.org>
2024-05-31 00:48:06 +0300

    Implement main loop

--- src/java_conversion_trainer/JavaConversionTrainerApp.java
+++ src/java_conversion_trainer/JavaConversionTrainerApp.java
@@ -1,7 +1,61 @@
 package java_conversion_trainer;
 
 class JavaConversionTrainerApp {
-
+	public static void main(String [] args)
+	{
+		run();
+	}
+	
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		java.util.Random rand = new java.util.Random();
+		
+		String [] types = {"byte", "short", "int", "long", "float", "double", "char", "boolean"};
+		
+		System.out.println("0 girerek çıkabilirsiniz.");
+		while (true) {
+			String src = types[rand.nextInt(types.length)];
+			String dst;
+			
+			do {
+				dst = types[rand.nextInt(types.length)];
+			} while (src.equals(dst));
+			
+			askQuestion(src, dst);
+			int answer = Integer.parseInt(kb.nextLine());
+			if (answer == 0)
+				break;
+			boolean isCorrect = isCorrectAnswer(src, dst, answer);
+			displayResult(isCorrect);
+			if (!isCorrect)
+				displayCorrectAnswer(src, dst);
+			System.out.println();
+		}
+	}
+	
+	public static void askQuestion(String src, String dst)
+	{
+		System.out.printf("'%s' türünden '%s' türüne doğrudan dönüşüm var mıdır?%n", src, dst);
+		System.out.println("1) Evet");
+		System.out.println("2) Hayır");
+	}
+	
+	public static boolean isCorrectAnswer(String src, String dst, int answer)
+	{
+		boolean isValid = TypeInfo.isValidImplicitConversion(src, dst);
+		return isValid && answer == 1 || !isValid && answer == 2;
+	}
+	
+	public static void displayResult(boolean isCorrect)
+	{
+		System.out.printf("%s%n", isCorrect ? "Tebrikler! Doğru cevap." : "Bir dahaki sefere...");
+	}
+	
+	public static void displayCorrectAnswer(String src, String dst)
+	{
+		System.out.printf("Doğru cevap: '%s'%n", TypeInfo.isValidImplicitConversion(src, dst) ? "Evet" : "Hayır");
+	}
 }
 
 class TypeInfo {
